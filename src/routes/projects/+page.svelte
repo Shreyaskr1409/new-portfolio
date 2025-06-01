fdc42868323c90377b8b12aac140192b8a224867
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2025-06-01 23:33:44 +0530

    Project page finished

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,13 +7,63 @@
     flex flex-col justify-start items-center p-4 pt-18 max-md:h-auto"
 >
 	<div
-		class="w-full bg-[#00000060] rounded-2xl
+		class="w-full rounded-2xl
         p-2 grid grid-cols-2 gap-2 max-2xl:grid-cols-1"
 	>
-		<Project></Project>
-		<Project></Project>
-		<Project></Project>
-		<Project></Project>
-		<Project></Project>
+		<Project
+			title="SimpliBlog"
+			repoURL="https://github.com/Shreyaskr1409/SimpliBlog"
+			secURL="https://simpliblog.site/"
+			tools="SvelteKit, JavaScript, NodeJS, MongoDB, Nginx"
+			body="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Optio quod nam, earum reprehenderit voluptas reiciendis nobis
+            aliquam molestias atque! Corporis at eos sit natus optio unde,
+            nesciunt id cum obcaecati."
+			background="bg-[url(/images/simpliblog.png)]"
+		></Project>
+		<Project
+			title="Chip-8 Emulator"
+			repoURL="https://github.com/Shreyaskr1409/go-chip8-emu"
+			secTitle="Releases"
+			secURL="https://github.com/Shreyaskr1409/go-chip8-emu/releases"
+			tools="Go, Ebitengine"
+			body="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Optio quod nam, earum reprehenderit voluptas reiciendis nobis
+            aliquam molestias atque! Corporis at eos sit natus optio unde,
+            nesciunt id cum obcaecati."
+			background="bg-[url(/images/chip-8.png)]"
+		></Project>
+		<Project
+			title="Nintendo Entertainment System (NES) Emulator"
+			repoURL="https://github.com/Shreyaskr1409/SmartClassroom/tree/main"
+			secURL="https://github.com/Shreyaskr1409/SmartClassroom/tree/main"
+			tools="C, SDL2"
+			body="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Optio quod nam, earum reprehenderit voluptas reiciendis nobis
+            aliquam molestias atque! Corporis at eos sit natus optio unde,
+            nesciunt id cum obcaecati."
+			background="bg-[url(/images/nes.png)]"
+		></Project>
+		<Project
+			title="RiskEye"
+			repoURL="https://github.com/Shreyaskr1409/RiskEye-Service"
+			secURL="https://documenter.getpostman.com/view/36354773/2sAXxY2TK1"
+			tools="NodeJS, JavaScript, Python"
+			body="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Optio quod nam, earum reprehenderit voluptas reiciendis nobis
+            aliquam molestias atque! Corporis at eos sit natus optio unde,
+            nesciunt id cum obcaecati."
+			background="bg-[url(/images/riskeye.jpg)]"
+		></Project>
+		<Project
+			title="Smart Classroom"
+			repoURL="https://github.com/Shreyaskr1409/SmartClassroom"
+			secURL="https://github.com/Shreyaskr1409/SmartClassroom"
+			tools="Go, Python"
+			body="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Optio quod nam, earum reprehenderit voluptas reiciendis nobis
+            aliquam molestias atque! Corporis at eos sit natus optio unde,
+            nesciunt id cum obcaecati."
+		></Project>
 	</div>
 </div>
--- src/routes/projects/project.svelte
+++ src/routes/projects/project.svelte
@@ -1,24 +1,49 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	export let title = '';
+	export let repoURL = '';
+	export let secTitle = 'Website';
+	export let secURL = '';
+	export let tools = '';
+	export let body = '';
+	export let background = 'bg-black';
+</script>
+
 <div
-	class="w-full flex flex-row min-h-[300px] p-2 backdrop-blur-md bg-[#00000020]
+	class="w-full flex flex-row min-h-[300px] p-2 backdrop-blur-md bg-[#00000080]
             rounded-2xl border-black border-2 gap-2 max-md:flex-col"
 >
 	<div
-		class="min-w-1/2 w-1/2 h-full bg-black self-stretch rounded-xl max-md:w-full max-md:h-[300px] max-[35rem]:h-[200px]"
+		class="min-w-1/2 w-1/2 h-full {background} bg-cover bg-center bg-top self-stretch rounded-xl max-md:w-full max-md:h-[300px] max-[35rem]:h-[200px]"
 	></div>
-	<div class="text-[#ffe4c4] font-raleway flex flex-col gap-1 items-start">
-		<h1 class="font-catchy text-4xl">SimpliBlog</h1>
+	<div class="text-[#ffe4c4] font-raleway flex flex-col gap-1 items-start grow">
+		<h1 class="font-catchy text-4xl">{title}</h1>
 		<div class="flex flex-row w-full gap-1">
-			<button class="bg-black cursor-pointer px-2 rounded-sm grow">GitHub</button>
-			<button class="bg-black cursor-pointer px-2 rounded-sm grow">Website</button>
+			<button
+				class="bg-black cursor-pointer px-2 rounded-sm grow"
+				on:click={() => {
+					window.open(repoURL, '_blank');
+				}}>GitHub</button
+			>
+			<button
+				class="bg-black cursor-pointer px-2 rounded-sm grow"
+				on:click={() => {
+					window.open(secURL, '_blank');
+				}}>{secTitle}</button
+			>
 		</div>
 		<h2 class="w-full border-y-black border-y-2">
-			Tools: NodeJS, SvelteKit, JavaScript, GitHub Actions
+			Tools: {tools}
 		</h2>
 		<p class="grow">
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae dolorum praesentium dolor
-			impedit voluptatum provident ipsum voluptates, quo quam iste accusantium nam nesciunt!
-			Dignissimos, facere! Quaerat distinctio optio ad vero!
+			{body}
 		</p>
-		<button class="bg-black cursor-pointer px-2 rounded-sm self-stretch">Show More...</button>
+		<button
+			class="bg-black cursor-pointer px-2 rounded-sm self-stretch"
+			on:click={() => {
+				window.open(repoURL, '_blank');
+			}}>Show More...</button
+		>
 	</div>
 </div>
